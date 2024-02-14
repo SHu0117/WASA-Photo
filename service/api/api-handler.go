2e9913762deeead8ffa225456efcd26c04245494
Siwei Hu <hu.1985972@studenti.uniroma1.it>
2024-02-14 20:32:48 +0100

    Fixed openapi document errors: content object added to 200 response and fixed type warnings. changed follwed to follower

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -14,7 +14,6 @@ func (rt *_router) Handler() http.Handler {
 	rt.router.POST("/users/:username/photos/", rt.wrap(rt.uploadPhoto))
 	rt.router.GET("/users/:username/photos/", rt.wrap(rt.getUserPhoto))
 	rt.router.DELETE("/users/:username/photos/:pid", rt.wrap(rt.deletePhoto))
-	// rt.router.GET("/users/:username/photos/:pid", rt.wrap(rt.getPhoto))
 
 	rt.router.PUT("/users/:username/photos/:pid/likes/:likeUsername", rt.wrap(rt.likePhoto))
 	rt.router.DELETE("/users/:username/photos/:pid/likes/:likeUsername", rt.wrap(rt.unlikePhoto))
